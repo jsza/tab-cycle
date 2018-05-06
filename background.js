96bf91242078a4166deb5670bbe4b1e79dbcf366
jayess <jayess@jayess.za.net>
2018-05-06 13:15:06 +0200

    Add next_tab function.

--- background.js
+++ background.js
@@ -1,6 +1,22 @@
 var extensionState = {"cycling": false}
 
 
+function next_tab() {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    // Sort tabs according to their index in the window.
+    tabs.sort((a, b) => { return a.index < b.index; });
+    let activeIndex = tabs.findIndex((tab) => { return tab.active; });
+    let lastTab = tabs.length - 1;
+    let newIndex = -1;
+    // if (command === 'flip-tabs-forward')
+    newIndex = activeIndex === 0 ? lastTab : activeIndex - 1;
+    // else  // 'flip-tabs-backwards'
+    //   newIndex = activeIndex === lastTab ? 0 : activeIndex + 1;
+    chrome.tabs.update(tabs[newIndex].id, {active: true, highlighted: true});
+  });
+};
+
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
